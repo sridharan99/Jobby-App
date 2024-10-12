77c02fcea318992622a455cdd653a334d1c9a4ef
sridharan99 <sridharanduraiyan@gmail.com>
2024-10-12 04:16:34 +0000

    first

--- src/components/ProfileDetails/index.js
+++ src/components/ProfileDetails/index.js
@@ -54,7 +54,7 @@ class ProfileDetails extends Component {
         <img src={profileImageUrl} alt="profile" className="profile-img" />
         <h1 className="profile-heading">{name}</h1>
         <p className="profile-bio">{shortBio}</p>
-        <h1 className="profile-heading">Koppolu Koushik</h1>
+        <h1 className="profile-heading">Sridharan</h1>
         <p className="profile-bio">Frontend Developer</p>
       </div>
     )
